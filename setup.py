50b165a9e3de19e1745d63bae0b319da33be4d25
Christoph Stelz <christoph.stelz@student.kit.edu>
2023-06-06 17:55:16 +0200

    Version 1.2.0
    New in this version:
    download older epapers, contributed by Dangertech

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name = "zeitdownload",
-        version = "1.0.3",
+        version = "1.1.0",
         author = "Christoph Stelz",
         author_email = "mail+python@ch-st.de",
         description = "Download the digital version of the newspaper \"Die Zeit\"",
